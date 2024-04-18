78e1c4ff326bb8f3caca0f4d10307f2cf40729f7
SeungHyun Hong <whi7ehyun@gmail.com>
2024-04-18 23:49:34 +0900

    refactor: async-await & 코드 리팩토링

--- Crawler/gs_crawler.py
+++ Crawler/gs_crawler.py
@@ -1,3 +1,7 @@
+import aiohttp
+import asyncio
+import json
+
 if __name__ == "__main__" or __name__ == "Crawler":
     from base.crawler import Crawler
     from event_items import EventItem
@@ -7,70 +11,72 @@ else:
     from .event_items import EventItem
     from .event_items import PromotionType
 
-import requests
-import json
-
 
 class GSCrawler(Crawler):
     def __init__(self):
         self.__url = "http://gs25.gsretail.com/gscvs/ko/products/event-goods-search"
         super().__init__()
 
-    def parse_data(self):
-        pass
+    def __parse_data(self, json_data) -> list[EventItem]:
+        event_items = []
+        for item in json_data["results"]:
+            event_item = EventItem(
+                promotion_type=(
+                    PromotionType.buy_one_get_one_free
+                    if item["eventTypeNm"] == "1+1"
+                    else PromotionType.buy_two_get_one_free
+                ),
+                event_name=item["goodsNm"],
+                price=item["price"],
+                name=item["abrGoodsNm"],
+                image_url=item["attFileNm"],
+            )
+            event_items.append(event_item)
+        return event_items
 
-    def execute(self):
+    async def __fetch_data(self, session, parameter_list, page_num, page_size):
+        params = {
+            "pageNum": page_num,
+            "pageSize": page_size,
+            "parameterList": parameter_list,
+        }
+        async with session.get(self.__url, params=params) as response:
+            return await json.loads(response.json())
+
+    async def execute(self):
         parameter_lists = ["ONE_TO_ONE", "TWO_TO_ONE"]
         page_size = 20
         data_array = []
 
-        for parameter_list in parameter_lists:
-            page_num = 1
-            while True:
-                params = {
-                    "pageNum": page_num,
-                    "pageSize": page_size,
-                    "parameterList": parameter_list,
-                }
-                response = requests.get(self.__url, params=params)
-
-                if response.status_code != 200:
-                    print(f"Error: {response.status_code}")
-                    break
-
-                # 받아온 데이터를 문자열로 변환
-                json_string = response.json()
-
-                # JSON 형식으로 변환
-                json_data = json.loads(json_string)
-
-                for item in json_data["results"]:
-                    event_item = EventItem(
-                        promotion_type=(
-                            PromotionType.buy_one_get_one_free
-                            if item["eventTypeNm"] == "1+1"
-                            else PromotionType.buy_two_get_one_free
-                        ),
-                        event_name=item["goodsNm"],
-                        price=item["price"],
-                        name=item["abrGoodsNm"],
-                        image_url=item["attFileNm"],
+        async with aiohttp.ClientSession() as session:
+            for parameter_list in parameter_lists:
+                page_num = 1
+                while True:
+                    json_data = await self.__fetch_data(
+                        session, parameter_list, page_num, page_size
                     )
+                    if "results" not in json_data:
+                        print("Error: Invalid response")
+                        break
 
-                    data_array.append(event_item)
-                    print(event_item)
+                    event_items = self.__parse_data(json_data)
+                    data_array.extend(event_items)
 
-                total_pages = json_data["pagination"]["numberOfPages"]
-                if page_num >= total_pages:
-                    break
+                    total_pages = json_data["pagination"]["numberOfPages"]
+                    if page_num >= total_pages:
+                        break
 
-                page_num += 1
+                    page_num += 1
 
         # 결과 출력
         print(f"Total data count: {len(data_array)}")
-        print(data_array)
+        return data_array
+
+
+async def main():
+    crawler = GSCrawler()
+    items = await crawler.execute()
 
 
 if __name__ == "__main__":
-    crawler = GSCrawler()
-    crawler.execute()
+    asyncio.run(main())
